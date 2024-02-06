46e1ceef495e2b80fcecb9230523b4c387e52c1b
Rajul <69156184+prepatdiscobj@users.noreply.github.com>
2024-02-07 01:19:16 +0530

    Sync blocked websites across devices
    Added functionality to sync the blocked website data across devices wherever extension is installed

--- src/background.js
+++ src/background.js
@@ -1,3 +1,22 @@
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ blockedWebsites: '' });
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  if (namespace === 'sync' && 'blockedWebsites' in changes) {
+    const newBlockedWebsites = changes.blockedWebsites.newValue;
+    const blockedWebsitesArray = newBlockedWebsites.split('\n');
+    chrome.scripting.executeScript({
+      target: { tabId: -1 }, // -1 targets all tabs
+      function: blockWebsites,
+      args: [blockedWebsitesArray]
+    });
+  }
 });
+
+function blockWebsites(blockedWebsites) {
+  const currentURL = window.location.href;
+
+  for (const website of blockedWebsites) {
+    if (currentURL.includes(website.trim())) {
+      window.location.href = chrome.runtime.getURL('blocked.html');
+      break;
+    }
+  }
+}
--- src/popup.js
+++ src/popup.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
   const blockedWebsitesInput = document.getElementById('blocked-websites');
   const saveButton = document.getElementById('save-button');
 
+  // Load blocked websites from sync storage
   chrome.storage.sync.get({ blockedWebsites: '' }, function (data) {
     blockedWebsitesInput.value = data.blockedWebsites;
   });
 
   saveButton.addEventListener('click', function () {
     const blockedWebsites = blockedWebsitesInput.value;
+
+    // Update blocked websites in sync storage
     chrome.storage.sync.set({ blockedWebsites });
   });
 });
